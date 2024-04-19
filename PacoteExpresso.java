5e1f09d96797a83d8e990560bbcc1929ed1c528e
Marya Luiza Rocha <84876774+MaryaLuizaRocha@users.noreply.github.com>
2024-04-19 16:12:47 -0300

    Delete PacoteExpresso.java

--- PacoteExpresso.java
+++
@@ -1,8 +0,0 @@
-package Pacotes;
-
-public class PacoteExpresso extends Pacote {
-    @Override
-    public double calcularCustoEnvio(double peso) {
-        return peso * 1.5; // Custo de envio expresso é 3 vezes o custo padrão
-    }
-}
