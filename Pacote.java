68ddcc908e9226f7a83ff390f1ebaa6fc4026f4f
Marya Luiza Rocha <84876774+MaryaLuizaRocha@users.noreply.github.com>
2024-04-19 16:12:35 -0300

    Delete Pacote.java

--- Pacote.java
+++
@@ -1,8 +0,0 @@
-package Pacotes;
-
-public class Pacote {
-    public double calcularCustoEnvio(double peso) {
-        // Implementação padrão para pacotes simples
-        return peso * 0.5; // Custo de envio padrão
-    }
-}
