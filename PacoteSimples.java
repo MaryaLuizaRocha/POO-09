068124bc82728e192b90368d42a7800be8677bab
Marya Luiza Rocha <84876774+MaryaLuizaRocha@users.noreply.github.com>
2024-04-19 16:12:53 -0300

    Delete PacoteSimples.java

--- PacoteSimples.java
+++
@@ -1,8 +0,0 @@
-package Pacotes;
-
-public class PacoteSimples extends Pacote {
-    @Override
-    public double calcularCustoEnvio(double peso) {
-        return peso * 0.5; // Custo de envio padrão para pacotes simples
-    }
-}
